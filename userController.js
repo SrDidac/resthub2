53efc840d750dbd6ef46c52022bfeb4361190940
Diego García Garijo <garciagarijodiego@gmail.com>
2019-10-17 18:57:12 +0200

    cambios en la clase

---
+++ userController.js
@@ -0,0 +1,78 @@
+// contactController.js
+// Import contact model
+User = require('./userModel');
+// Handle index actions
+exports.index = function (req, res) {
+    User.get(function (err, user) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "Users retrieved successfully",
+            data: users
+        });
+    });
+};
+// Handle create user actions
+exports.new = function (req, res) {
+    var user = new User();
+    user.name = req.body.name ? req.body.name : user.name;
+    user.pass = req.body.pass;
+// save the contact and check for errors
+    user.save(function (err) {
+        // Check for validation error
+        if (err)
+            res.json(err);
+        else
+            res.json({
+                message: 'New user created!',
+                data: user
+            });
+    });
+};
+// Handle view contact info
+exports.view = function (req, res) {
+    User.findById(req.params.user_id, function (err, user) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'User details loading..',
+            data: user
+        });
+    });
+};
+// Handle update contact info
+exports.update = function (req, res) {
+    User.findById(req.params.user_id, function (err, user) {
+        if (err)
+            res.send(err);
+        user.name = req.body.name ? req.body.name : user.name;
+        user.pass = req.body.pass;
+// save the contact and check for errors
+        user.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                message: 'User Info updated',
+                data: user
+            });
+        });
+    });
+};
+// Handle delete contact
+exports.delete = function (req, res) {
+    User.remove({
+        _id: req.params.user_id
+    }, function (err, user) {
+        if (err)
+            res.send(err);
+        res.json({
+            status: "success",
+            message: 'User deleted'
+        });
+    });
+};
---
+++ userModel.js
@@ -0,0 +1,18 @@
+// contactModel.js
+var mongoose = require('mongoose');
+// Setup schema
+var userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    pass: {
+        type: String,
+        required: true
+    }
+});
+// Export Contact model
+var User = module.exports = mongoose.model('user', userSchema);
+module.exports.get = function (callback, limit) {
+    User.find(callback).limit(limit);
+}
